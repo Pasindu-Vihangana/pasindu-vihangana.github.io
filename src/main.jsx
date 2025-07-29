ee591c372cb018d3bc688d6679a6b1f327b6ce25
Pasindu Vihangana <pasi1028@gmail.com>
2025-07-29 06:06:46 +0530

    update in progress added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.jsx';
 import AboutPage from './components/AboutPage.jsx';
 import ContactPage from './components/ContactPage.jsx';
 import CertificationsPage from './components/CertificationsPage.jsx';
 import ProjectsPage from './components/ProjectsPage.jsx';
+import UpdateInProgress from './components/UpdateInProgress.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -15,8 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/certifications" element={<CertificationsPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/certifications" element={<Navigate to="/update-in-progress" replace />} />
+        <Route path="/projects" element={<Navigate to="/update-in-progress" replace />} />
+        <Route path="/update-in-progress" element={<UpdateInProgress />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
